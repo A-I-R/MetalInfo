e6cc5dc86a75c9ac38f8a8031f642dc84a2e012f
A-I-R <876018382@qq.com>
2015-06-14 22:54:54 +0800

    The second version

--- src/com/air/main/Main.java
+++ src/com/air/main/Main.java
@@ -2,6 +2,7 @@ package com.air.main;
 
 import java.util.Scanner;
 
+import com.air.function.SimilarityFactory;
 import com.air.function.WordFrequencyFactory;
 import com.air.function.KeywordFactory;
 import com.air.function.TFIDFFactory;
@@ -14,7 +15,7 @@ public class Main {
 		// TODO Auto-generated method stub
 
 		System.out.println("欢迎！");
-		System.out.println("请输入指令：‘kw’：关键词词频统计；‘ab’：摘要分词并统计；‘ti’：统计tf-idf");
+		System.out.println("请输入指令：‘kw’：关键词词频统计；‘ab’：摘要分词并统计；‘ti’：统计tf-idf；‘sm’：计算词频余弦夹角相似度");
 		Scanner scanner=new Scanner(System.in);
 		switch(scanner.nextLine())
 		{
@@ -38,7 +39,12 @@ public class Main {
 			tf.getTFIDF(target+"摘要词频.txt", target+"tf-idf.txt");
 			break;
 		}
-		
+		case "sm":
+		{
+			SimilarityFactory sf=new SimilarityFactory();
+			sf.getAngleSimilarity(target+"摘要词频.txt", target+"similarity.txt");
+			break;
+		}
 		}
 		
 		scanner.close();
